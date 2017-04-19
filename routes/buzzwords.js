aa6b3ed389e90668c6d6cf717477ce1e4e1ef80e
Adam <acdoyle630@gmail.com>
2017-04-18 19:29:43 -1000

    post complete hp, no retrun false on repeat or unheard

--- routes/buzzwords.js
+++ routes/buzzwords.js
@@ -3,35 +3,43 @@
 const express = require('express');
 const router = express. Router();
 
-let buzzwords = { buzzwords:[ ] };
+
+let buzzObject = { buzzwords:[ ] };
 let score = 0;
 
 module.exports = router;
 
 router.route('/')
   .get(( req, res ) =>{
-    res.json(buzzwords);
+    res.json(buzzObject);
   })
   .post(( req, res )=>{
-    console.log(req.body.buzzword);
-    console.log(buzzwords.buzzwords);
-    if(checkBuzzArray(req.body.buzzword) === true ){   buzzwords.buzzwords.push(req.body.buzzword);
-    console.log(buzzwords);
-    res.json({success: true});
+    if(checkBuzzArray(req.body.buzzword) < 0 ){
+      buzzObject.buzzwords.push(req.body);
+      res.json({'success': true});
     } else {
-      res.json({success: false});
+      res.json({'success': false});
     }
   })
   .put(( req, res )=>{
-    console.log('hit put');
-  });
+    let index = checkBuzzArray(req.body.buzzword);
+    if(index > -1){
+      current = buzzObject.buzzwords[index];
+      current.heard = req.body.heard;
+      score += (Number(current.points));
+      res.json({'success': true, 'newScore': score });
+      console.log(current);
+      console.log(buzzObject);
+    }
+});
 
 
   function checkBuzzArray(word){
-    console.log(word);
-    if(buzzwords.buzzwords.indexOf(word) === -1){
-      console.log('NOt found');
-      console.log(buzzwords.buzzwords);
-      return true;
+    if (buzzObject.buzzwords.length === 0){
+      return -1;
+    } else {
+      for(var i = 0; i<buzzObject.buzzwords.length; i++){
+        return(buzzObject.buzzwords[i].buzzword.indexOf(word));
+      }
     }
   }
